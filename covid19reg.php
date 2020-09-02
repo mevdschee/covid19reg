a36c38126d57c30e30443e3725db692d628199f4
Maurits van der Schee <maurits@vdschee.nl>
2020-09-02 19:56:18 +0200

    update

--- covid19reg.php
+++ covid19reg.php
@@ -12,14 +12,14 @@ echo "<html><head><title>$texts[title]</title></head><body>";
 echo '<div style="float:right"><a href="qrcode.php">QR</a></div>';
 echo "<h1>$texts[title]</h1>";
 $identification = $_COOKIE['covid19reg'] ?? $_POST['covid19reg'] ?? false;
+$day = date('d');
+$date = date('Y-m-d');
+$time = date('H:i:s');
+$file = file_get_contents("$day.php");
+if (substr($file, 17, 10) != $date) {
+    file_put_contents("$day.php", "<?php exit(); // $date\n");
+}
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    $day = date('d');
-    $date = date('Y-m-d');
-    $time = date('H:i:s');
-    $file = file_get_contents("$day.php");
-    if (substr($file, 17, 10) != $date) {
-        file_put_contents("$day.php", "<?php exit(); // $date\n");
-    }
     file_put_contents("$day.php", "// $time $identification\n", FILE_APPEND);
     setcookie('covid19reg', $identification, time() + 24 * 3600 * 365);
     echo '<form>';
