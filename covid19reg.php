50c6b541889ad1f1ed21bd308bcc0f4d5defd56d
Maurits van der Schee <maurits@vdschee.nl>
2020-09-02 14:51:26 +0200

    update

--- covid19reg.php
+++ covid19reg.php
@@ -1,10 +1,17 @@
-<html>
-<head><title>COVID19 registration</title></head>
-<body>
-<h1>COVID19 registration</h1>
 <?php
+// texts
+$texts = [
+    'title' => 'COVID19 registration',
+    'registered' => 'You have been registered as: "%s".',
+    'label' => 'Phone number / email address',
+    'next' => 'Next',
+    'register' => 'Register',
+];
+// code
+echo "<html><head><title>$texts[title]</title></head>";
+echo "<body><h1>$texts[title]</h1>";
 $identification = $_COOKIE['covid19reg'] ?? $_POST['covid19reg'] ?? false;
-if ($_SERVER["REQUEST_METHOD"] == 'POST'):
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $day = date('d');
     $date = date('Y-m-d');
     $time = date('H:i:s');
@@ -14,19 +21,15 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'):
     }
     file_put_contents("$day.log", "$time $identification\n", FILE_APPEND);
     setcookie('covid19reg', $identification, time() + 24 * 3600 * 365);
-    ?>
-    <form>
-    <p>You have been registered as: "<?php echo htmlspecialchars($identification, ENT_QUOTES, 'UTF-8'); ?>".</p>
-    <input type="submit" value="Ok">
-    </form>
-    <?php
-else: ?>
-    <form method="post">
-    <p>Enter a phone number or email address, so that we can contact you.</p>
-    <input name="covid19reg" value="<?php echo htmlspecialchars($identification, ENT_QUOTES, 'UTF-8'); ?>">
-    <input type="submit" value="Register">
-    </form>
-    <?php
-endif;
-?>
-</html>
+    echo '<form>';
+    echo sprintf($texts['registered'], htmlspecialchars($identification, ENT_QUOTES, 'UTF-8'));
+    echo '<input type="submit" value="' . $texts['next'] . '">';
+    echo '</form>';
+} else {
+    echo '<form method="post">';
+    echo '<label for="covid19reg">' . $texts['label'] . '</label><br/>';
+    echo '<input id="covid19reg" name="covid19reg" value="' . htmlspecialchars($identification, ENT_QUOTES, 'UTF-8') . '">';
+    echo '<input type="submit" value="' . $texts['register'] . '">';
+    echo '</form>';
+}
+echo '</body></html>';
