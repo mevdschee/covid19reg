f121a34a4dc789a63e29306d3a7e78ed2000aa39
Maurits van der Schee <maurits@vdschee.nl>
2020-09-02 14:30:14 +0200

    update

--- covid19reg.php
+++ covid19reg.php
@@ -1,15 +1,25 @@
 <html>
-<head><title></title></head>
+<head><title>COVID19 registration</title></head>
 <body>
 <h1>COVID19 registration</h1>
 <?php
 $identification = $_COOKIE['covid19reg'] ?? $_POST['covid19reg'] ?? false;
-if ($identification):
+if ($_SERVER["REQUEST_METHOD"] == 'POST'):
+    $day = date('d');
     $date = date('Y-m-d');
     $time = date('H:i:s');
-    file_put_contents()
+    $file = file_get_contents("$day.log");
+    if (substr($file, 0, 10) != $date) {
+        file_put_contents("$day.log", "$date\n");
+    }
+    file_put_contents("$day.log", "$time $identification\n", FILE_APPEND);
     setcookie('covid19reg', $identification, time() + 24 * 3600 * 365);
-    echo '<p>You have been registered as "' . htmlspecialchars($identification, ENT_QUOTES, 'UTF-8') . '".</p>';
+    ?>
+    <form>
+    <p>You have been registered as: "<?php echo htmlspecialchars($identification, ENT_QUOTES, 'UTF-8'); ?>".
+    <input type="submit" value="Ok">
+    </form>
+    <?php
 else: ?>
     <form method="post">
     <input name="covid19reg" value="<?php echo htmlspecialchars($identification, ENT_QUOTES, 'UTF-8'); ?>">
