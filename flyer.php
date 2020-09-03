ff98835f7424622dc5f9326d74c1a389bc676130
Maurits van der Schee <maurits@vdschee.nl>
2020-09-04 00:30:48 +0200

    svg

--- flyer.php
+++ flyer.php
@@ -1,5 +1,6 @@
 <?php
 $url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+$url = str_replace('flyer.php', '', $url);
 ?>
 
 <div><img src="qrcode.php"></div>
